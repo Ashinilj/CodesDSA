b79ed8da094f1c5a55296e01aae419e58dc71b35
Ashini L J <156600873+Ashinilj@users.noreply.github.com>
2025-07-20 23:20:26 +0530

    Create Recursion_pow(x,n).cpp

---
+++ Recursion_pow(x,n).cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+       if(n==0) return 1;
+       else if(n==1) return x;
+
+       long long N= n;
+       if(N<0){
+        x= 1/x;
+        N= -N;
+       }
+
+       double half= myPow(x, N/2);
+       if(N%2==0) return half*half;
+       else return half*half*x;
+    }
+};
